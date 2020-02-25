bd69849a7c10358663f505a49458617c1868ad56
periish <sem3colon@gmail.com>
2020-02-25 19:14:35 +0000

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+# Installation script for sea
+
+log () {
+    # print fancy outputs
+    printf "%b\n" "\u001b[36m[#] \u001b[0m$1"
+}
+
+die () {
+    # exit program with 1
+    printf "%b\n" "\u001b[35m[x] \u001b[1m\u001b[31m$1"
+    exit 1
+}
+
+log "Checking for cURL..."
+command -v curl || die "cURL not present. Please install it with your package manager."
+log "cURL present."
+
+log "Checking for tar..."
+command -v tar || die "tar not present. Please install it with your package manager."
+log "tar present."
+
+# Actually start the install.
+
+[ "$(id -u)" != 0 ] || die "Script must be run as root."
+cp sea /usr/bin
+# TODO: Add documentation then install it.
