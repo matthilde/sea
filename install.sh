d09643f7f001cfcae4c7d9e09dc64bc28afb0260
periish <sem3colon@gmail.com>
2020-03-18 13:54:04 +0000

    Get mandb and command -v to stop printing.

--- install.sh
+++ install.sh
@@ -16,20 +16,20 @@ die () {
 [ "$(id -u)" = 0 ] || die "Script must be run as root."
 
 log "Checking for cURL..."
-command -v curl || die "cURL not present. Please install it with your package manager."
+command -v curl >/dev/null || die "cURL not present. Please install it with your package manager."
 log "cURL present."
 
 log "Checking for tar..."
-command -v tar || die "tar not present. Please install it with your package manager."
+command -v tar >/dev/null || die "tar not present. Please install it with your package manager."
 log "tar present."
 
 log "Checking for gzip..."
-command -v gzip || die "gzip not present. Please install it with your package manager."
+command -v gzip >/dev/null || die "gzip not present. Please install it with your package manager."
 # Actually start the install.
 
 install -Dm755 sea "$PREFIX/bin"
 # Do not install man if it is not installed.
-command -v man && {
+command -v man >/dev/null && {
     gzip -c sea.1>/usr/share/man/man1/sea.1.gz
-    mandb
+    mandb >/dev/null
 }
